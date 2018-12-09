941ca0b0c21a8774584faf5fc33a43bb413b5477
Yvesyule <mwumvanezayves@gmail.com>
2018-12-09 02:09:49 -0800

    procedure

---
+++ procedure.py
@@ -0,0 +1,12 @@
+def tentimestable():
+    for x in range(10): 
+        print(x, "x multiplied by 10 equal ", x*10)
+
+def fivetimestable():
+   for x in range(5): 
+        print(x, "x multiplied by 5 equal ", x*5)
+
+#main program
+tentimestable()
+fivetimestable()
+
